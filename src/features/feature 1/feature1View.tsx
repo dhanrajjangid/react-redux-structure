c2f751c613530a5514dd502bfe12846189e7fbb6
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2024-01-12 18:26:43 +0530

    first commit

---
+++ src/features/feature 1/feature1View.tsx
@@ -0,0 +1,13 @@
+// Feature1View.tsx
+import React from 'react';
+import './Feature1Styles.css'; // Import styles
+
+const Feature1View: React.FC = () => {
+  return (
+    <div className="feature1-container">
+      {/* Your view content */}
+    </div>
+  );
+};
+
+export default Feature1View;
